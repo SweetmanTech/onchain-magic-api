ccb9f6fb74eff230151a97ea2d0e83cac3fe5e59
Sweets Sweetman <sweetmantech@gmail.com>
2023-12-03 20:58:35 -0300

    I refacor

--- pages/api/decode/multicall/index.tsx
+++ pages/api/decode/multicall/index.tsx
@@ -1,23 +1,19 @@
-/* eslint-disable no-console */
-
 import getDecodedMulticall from "../../../../lib/getDecodedMulticall"
 
 const decodedMulticall = async (calls = []) => {
   let response
   try {
-    console.log("SWEETS HELLO WORLD", calls)
     const decodedCalls = calls.map((call) => getDecodedMulticall(call))
-
     response = { calls, decodedCalls }
   } catch (ex) {
     response = { data: false }
+    // eslint-disable-next-line no-console
     console.error(ex)
   }
   return response
 }
 
 export default async function handler(req: any, res: any) {
-  console.log("SWEETS req", req.query.multicalls)
   const data = await decodedMulticall(req?.query?.multicalls)
   res.status(200).json(data)
 }
